2676030cd2028d495e0fa3fc65f7c3379056b836
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-03 15:14:31 +0100

    Added search button and search function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 function run(){
     moveMapTo(-0.1276250, 51.5033630);
 }
+
+function search(){
+    console.log("# search()")
+    var job_area = document.getElementById("job_area");
+    var job_type = document.getElementById("job_type");
+    var map_type = document.getElementById("map_type");
+    console.log("job area: " + job_area);
+    console.log("job type: " + job_type);
+    console.log("map type: " + map_type);
+}
