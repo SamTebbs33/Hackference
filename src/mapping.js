29f5cd776d8b079272f6ff59e9a8fe3744230f03
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-03 15:02:35 +0100

    The map-related functions are now assigned to variables

--- src/mapping.js
+++ src/mapping.js
@@ -1,19 +1,25 @@
 // source: https://developers.arcgis.com/javascript/jsapi/map-amd.html
 var map;
+
+var moveMapTo;
+var setBaseMap;
+
 //var Point = require('esri/geometry/point');
-require(["esri/map", "dojo/domReady!", "esri/geometry/Point"], function(Map, Point) {
+require(["esri/map", "esri/geometry/Point","dojo/domReady!"], function(Map, Point) {
   map = new Map("map", {
     center: [-56.049, 38.485],
     zoom: 3,
     basemap: "streets"
   });
   run();
+
+    moveMapTo = function(x, y){
+        map.centerAt(new Point(x, y));
+    }
+
+    setBaseMap = function(basemap){
+         map.setBaseMap(basemap);
+    }
+
+
 });
-
-function moveMapTo(x, y){
-    map.centerAt(new Point(x, y));
-}
-
-function setBaseMap(basemap){
-    map.setBaseMap(basemap);
-}
