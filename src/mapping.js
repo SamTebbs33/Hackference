b7422d68e33888e4a3202263735845fa90c45e8e
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-03 14:32:40 +0100

    More debugging (what a surprise!)

--- src/mapping.js
+++ src/mapping.js
@@ -1,11 +1,12 @@
 // source: https://developers.arcgis.com/javascript/jsapi/map-amd.html
 
-require(["esri/map", "dojo/domReady!"], function(esriMap) {
+require(["esri/map", "dojo/domReady!"], function(Map) {
   map = new Map("map", {
     center: [-56.049, 38.485],
     zoom: 3,
     basemap: "streets"
   });
+  Map = map;
 });
 
 function moveMapTo(x, y){
