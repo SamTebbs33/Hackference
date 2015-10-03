18201154842628b87b8cebeb99249873c8cb1702
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-03 14:30:21 +0100

    More debugging

--- src/mapping.js
+++ src/mapping.js
@@ -1,6 +1,6 @@
 // source: https://developers.arcgis.com/javascript/jsapi/map-amd.html
 
-require(["esri/map", "dojo/domReady!"], function(Map) {
+require(["esri/map", "dojo/domReady!"], function(esriMap) {
   map = new Map("map", {
     center: [-56.049, 38.485],
     zoom: 3,
